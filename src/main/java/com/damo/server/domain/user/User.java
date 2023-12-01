be626ef02a2a228155b4388378c97dee3693987a
hoonloper <yonghoonj98@gmail.com>
2023-12-01 20:08:09 +0900

    Feat: Entity 프로퍼티 수정

--- src/main/java/com/damo/server/domain/user/User.java
+++ src/main/java/com/damo/server/domain/user/User.java
@@ -1,9 +1,6 @@
 package com.damo.server.domain.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,23 +19,27 @@ public class User {
     private String username; // 고유 식별값
 
     private String name;
+
     private String email;
+
     private String role;
+
     private String provider; // TODO: enum으로 변경 / google, kakao, naver
+
+    @Column(name = "provider_id")
     private String providerId;
 
     @CreationTimestamp
-    private Timestamp createdDate;
-
+    @Column(name = "created_at")
+    private Timestamp createdAt;
 
     @Builder
-    public User(String username, String name, String email, String role, String provider, String providerId, Timestamp createdDate) {
+    public User(String username, String name, String email, String role, String provider, String providerId) {
         this.username = username;
         this.name = name;
         this.email = email;
         this.role = role;
         this.provider = provider;
         this.providerId = providerId;
-        this.createdDate = createdDate;
     }
 }
