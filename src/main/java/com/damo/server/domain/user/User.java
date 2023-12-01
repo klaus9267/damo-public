1ab52240eaf11de0eaa918a4c0d4467676dd815b
hoonloper <yonghoonj98@gmail.com>
2023-12-01 20:04:35 +0900

    Feat: id int 타입을 Long으로 수정

--- src/main/java/com/damo/server/domain/user/User.java
+++ src/main/java/com/damo/server/domain/user/User.java
@@ -17,7 +17,7 @@ import java.sql.Timestamp;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private String username; // 고유 식별값
 
--- src/main/java/com/damo/server/domain/user/UserRepository.java
+++ src/main/java/com/damo/server/domain/user/UserRepository.java
@@ -2,6 +2,6 @@ package com.damo.server.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByProviderAndProviderId(String provider, String providerId);
 }
