4c3e13a78a62067778e432a0137e207fbf69eb6c
hoonloper <yonghoonj98@gmail.com>
2023-12-07 19:45:12 +0900

    Feat: 응답 타입을 ResponstEntity로 변경

--- src/main/java/com/damo/server/application/controller/UserController.java
+++ src/main/java/com/damo/server/application/controller/UserController.java
@@ -4,6 +4,8 @@ package com.damo.server.application.controller;
 import com.damo.server.domain.person.dto.PersonDto;
 import com.damo.server.domain.user.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,8 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("{id}/persons")
-    public List<PersonDto> readPersonsByUserId(@PathVariable("id") final Long userId) {
-        return userService.readPersonsByUserId(userId);
+    public ResponseEntity<?> readPersonsByUserId(@PathVariable("id") final Long userId) {
+        List<PersonDto> people = userService.readPersonsByUserId(userId);
+        return new ResponseEntity<>(people, HttpStatus.OK);
     }
 }
