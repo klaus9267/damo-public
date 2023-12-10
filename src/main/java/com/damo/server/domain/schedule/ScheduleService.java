da1c3df92076a872309f6be8679e02a2762ece8c
klaus9267 <klaus9267@gmail.com>
2023-12-11 00:52:26 +0900

    Refactor: 매개변수에 final 추가 및 exception 수정

--- src/main/java/com/damo/server/domain/schedule/ScheduleService.java
+++ src/main/java/com/damo/server/domain/schedule/ScheduleService.java
@@ -3,13 +3,14 @@ package com.damo.server.domain.schedule;
 import com.damo.server.domain.schedule.dto.ScheduleDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
 
 @AllArgsConstructor
 @Service
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
 
-    public ScheduleDto readSchedule(Long scheduleId) {
-        return scheduleRepository.findScheduleById(scheduleId).orElseThrow(()->new RuntimeException("조회할 대상을 찾을 수 없음"));
+    public ScheduleDto readSchedule(final Long scheduleId) {
+        return scheduleRepository.findOne(scheduleId).orElseThrow(() -> new NotFoundException("조회할 대상을 찾을 수 없음"));
     }
 }
