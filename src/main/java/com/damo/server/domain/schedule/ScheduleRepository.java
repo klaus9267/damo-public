4b6b4f38ed23e470092ed8b1347a52b3b472dfa0
klaus9267 <klaus9267@gmail.com>
2023-12-11 00:51:16 +0900

    Refactor: findOne query 수정, @Param 어노테이션 추가

--- src/main/java/com/damo/server/domain/schedule/ScheduleRepository.java
+++ src/main/java/com/damo/server/domain/schedule/ScheduleRepository.java
@@ -3,10 +3,11 @@ package com.damo.server.domain.schedule;
 import com.damo.server.domain.schedule.dto.ScheduleDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    @Query("SELECT new com.damo.server.domain.schedule.dto.ScheduleDto(p) FROM Schedule p  WHERE p.id = :scheduleId")
-    Optional<ScheduleDto> findScheduleById(Long scheduleId);
+    @Query("SELECT new com.damo.server.domain.schedule.dto.ScheduleDto(s, p) FROM Schedule s LEFT JOIN FETCH  Person p ON s.person.id = p.id WHERE s.id = :scheduleId")
+    Optional<ScheduleDto> findOne(@Param("scheduleId") Long scheduleId);
 }
