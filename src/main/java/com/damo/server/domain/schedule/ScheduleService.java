e6866e72dab8f4a80d7a08697e04e724a9af9dac
klaus9267 <klaus9267@gmail.com>
2023-12-11 19:15:20 +0900

    Feat: 스케줄 삭제 시 스케줄 존재 여부 판단 조건 추가

--- src/main/java/com/damo/server/domain/schedule/ScheduleService.java
+++ src/main/java/com/damo/server/domain/schedule/ScheduleService.java
@@ -6,6 +6,7 @@ import com.damo.server.domain.person.dto.PersonDto;
 import com.damo.server.domain.person.dto.RequestPersonDto;
 import com.damo.server.domain.schedule.dto.RequestScheduleDto;
 import com.damo.server.domain.schedule.dto.ScheduleDto;
+import com.damo.server.domain.schedule.entity.Schedule;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,6 +31,7 @@ public class ScheduleService {
     }
 
     public void removeScheduleById(final Long scheduleId) {
+        this.readSchedule(scheduleId);
         // TODO: security로 personId 받으면 같은 유저인지 판단 조건 추가
         scheduleRepository.deleteById(scheduleId);
     }
