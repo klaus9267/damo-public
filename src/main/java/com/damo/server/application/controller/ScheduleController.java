978a6a68dfd50df2f2ddafed5584e2ad7092fb57
klaus9267 <klaus9267@gmail.com>
2023-12-18 11:47:47 +0900

    Feat: 응답방식 수정 및 불필요한 swagger 정리

--- src/main/java/com/damo/server/application/controller/ScheduleController.java
+++ src/main/java/com/damo/server/application/controller/ScheduleController.java
@@ -1,19 +1,21 @@
 package com.damo.server.application.controller;
 
+import com.damo.server.application.config.oauth.PrincipalDetails;
 import com.damo.server.domain.common.pagination.CustomSchedulePage;
 import com.damo.server.domain.common.pagination.param.SchedulePaginationParam;
 import com.damo.server.domain.schedule.ScheduleService;
 import com.damo.server.domain.schedule.dto.RequestScheduleDto;
 import com.damo.server.domain.schedule.dto.ScheduleDto;
+import com.damo.server.domain.schedule.entity.ScheduleTransaction;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
@@ -24,11 +26,10 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     @PostMapping
-    @Operation(summary = "스케줄 생성")
-    @ApiResponse(responseCode = "201", useReturnTypeSchema = true)
-    public ResponseEntity<ScheduleDto> save(@RequestBody final RequestScheduleDto scheduleDto) {
-        // TODO: userId는 security에서 제공하는 데이터로 변경
-        return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.save(scheduleDto));
+    @Operation(summary = "스케줄 생성", description = "응답 없음")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addSchedule(@RequestBody final RequestScheduleDto scheduleDto) {
+        scheduleService.save(scheduleDto);
     }
 
     @GetMapping("{scheduleId}")
@@ -43,31 +44,32 @@ public class ScheduleController {
     @Operation(summary = "스케줄 타입별 목록 조회")
     @ApiResponse(responseCode = "200", useReturnTypeSchema = true)
     public ResponseEntity<CustomSchedulePage> readScheduleList(
-            @Valid
-            final SchedulePaginationParam paginationParam,
-            @Parameter(name = "transaction",description = "조회할 스케줄 종류", example = "RECEIVING")
+            @Valid final SchedulePaginationParam paginationParam,
+            @Parameter(name = "transaction", description = "조회할 스케줄 종류", example = "RECEIVING")
             @RequestParam(required = true)
-            @Length(max = 20) final String transaction
+            final ScheduleTransaction transaction,
+            @AuthenticationPrincipal PrincipalDetails principalDetails
     ) {
         // TODO: userId는 security에서 제공하는 데이터로 변경
-        final CustomSchedulePage schedule = scheduleService.readScheduleList(paginationParam.toPageable(), 1L, transaction);
+        final CustomSchedulePage schedule = scheduleService.readScheduleList(paginationParam.toPageable(), 4L, transaction);
+//        final CustomSchedulePage schedule = scheduleService.readScheduleList(paginationParam.toPageable(), principalDetails.getUser().getId(), transaction);
         return ResponseEntity.ok(schedule);
     }
 
     @PatchMapping("{scheduleId}")
-    @Operation(summary = "스케줄 수정")
-    @ApiResponse(responseCode = "200", useReturnTypeSchema = true)
-    public ResponseEntity<?> patchScheduleById(
+    @Operation(summary = "스케줄 수정", description = "응답 없음")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void patchScheduleById(
             @RequestBody final RequestScheduleDto scheduleDto,
-            @PathVariable("scheduleId") final Long scheduleId) {
-        return ResponseEntity.ok(scheduleService.patchScheduleById(scheduleDto, scheduleId));
+            @PathVariable("scheduleId") final Long scheduleId
+    ) {
+        scheduleService.patchScheduleById(scheduleDto, scheduleId);
     }
 
     @DeleteMapping("{scheduleId}")
     @Operation(summary = "스케줄 삭제", description = "응답 없음")
-    @ApiResponse(responseCode = "204")
-    public ResponseEntity<?> removeScheduleById(@PathVariable("scheduleId") final Long scheduleId) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeScheduleById(@PathVariable("scheduleId") final Long scheduleId) {
         scheduleService.removeScheduleById(scheduleId);
-        return ResponseEntity.noContent().build();
     }
 }
