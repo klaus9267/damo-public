25f08b0bf1f5922082446c0fec31ad28143af1bf
hoonloper <yonghoonj98@gmail.com>
2023-12-20 19:23:51 +0900

    Feat: 유저의 대상 조회 API 엔드포인트 수정 및 스웨거 등록

--- src/main/java/com/damo/server/application/controller/UserController.java
+++ src/main/java/com/damo/server/application/controller/UserController.java
@@ -1,26 +1,31 @@
 package com.damo.server.application.controller;
 
 
+import com.damo.server.application.config.oauth.PrincipalDetails;
 import com.damo.server.domain.person.dto.PersonDto;
 import com.damo.server.domain.user.UserService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "USER")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("{userId}/persons")
-    public ResponseEntity<?> readPersonsByUserId(@PathVariable("userId") final Long userId) {
-        List<PersonDto> people = userService.readPersonsByUserId(userId);
+    @ApiResponse(responseCode = "200", description = "유저가 등록한 대상 목록 조회", useReturnTypeSchema = true)
+    @GetMapping("me/persons")
+    public ResponseEntity<List<PersonDto>> readPersonsByUserId(@AuthenticationPrincipal final PrincipalDetails principalDetails) {
+        List<PersonDto> people = userService.readPersonsByUserId(principalDetails.getUser().getId());
         return ResponseEntity.ok(people);
     }
 }
