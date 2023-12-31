9c968d3addda915a91b63bc30db74a94ffe8afb2
hoonloper <yonghoonj98@gmail.com>
2023-12-31 15:47:31 +0900

    Feat: 사용하지 않는 API 제거

--- src/main/java/com/damo/server/application/controller/OAuthController.java
+++ src/main/java/com/damo/server/application/controller/OAuthController.java
@@ -8,25 +8,15 @@ import com.damo.server.domain.user.dto.UserDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("oauth")
 public class OAuthController {
     private final JwtTokenService jwtTokenService;
-
-    // OAuth 연동 확인을 위한 임시 API
-    @GetMapping
-    public Map<String, Object> tempOAuth2(@AuthenticationPrincipal OAuth2User oauth) {
-        return oauth.getAttributes();
-    }
 
     @GetMapping("token/expired")
     public boolean checkExpiredToken(final HttpServletRequest request) {
