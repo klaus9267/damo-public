fd404f8e0ec5bb0665a193195a0df933334fb9b8
hoonloper <yonghoonj98@gmail.com>
2023-12-31 15:29:11 +0900

    Setting: Swagger에 JWT 토큰 적용

--- src/main/java/com/damo/server/application/config/SwaggerConfig.java
+++ src/main/java/com/damo/server/application/config/SwaggerConfig.java
@@ -1,21 +1,40 @@
 package com.damo.server.application.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-        info = @io.swagger.v3.oas.annotations.info.Info(
+        info = @Info(
                 title = "DAMO API",
                 description = "Damo's API",
-                version = "v0.1.0")
+                version = "v0.1.0"
+        ),
+        security = @SecurityRequirement(name = "bearer-token")
 )
+@SecuritySchemes({
+        @SecurityScheme(
+                name = "bearer-token",
+                type = SecuritySchemeType.HTTP,
+                description = "Header param - Authorization",
+                scheme = "bearer",
+                bearerFormat = "JWT",
+                in = SecuritySchemeIn.HEADER,
+                paramName = "Authorization"
+        ),
+})
 @Configuration
 public class SwaggerConfig {
     @Bean
     public GroupedOpenApi openAPI() {
-        String[] paths = {"/api/**"};
+        String[] paths = {"/api/**", "/oauth/**"};
         return GroupedOpenApi.builder()
                 .group("DAMO API")
                 .pathsToMatch(paths)
