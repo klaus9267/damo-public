b678d39c3ac6a769bad88ee046799d1c1590ade8
klaus9267 <klaus9267@gmail.com>
2024-01-14 22:00:12 +0900

    Feat: 일정 스웨거 적용

--- src/main/java/com/damo/server/application/controller/ScheduleController.java
+++ src/main/java/com/damo/server/application/controller/ScheduleController.java
@@ -1,5 +1,9 @@
 package com.damo.server.application.controller;
 
+import com.damo.server.application.controller.operation.schedule.CreateScheduleOperationWithBody;
+import com.damo.server.application.controller.operation.schedule.ScheduleOperationWithBody;
+import com.damo.server.application.controller.operation.schedule.ScheduleOperationWithNoBody;
+import com.damo.server.application.controller.operation.schedule.UpdateScheduleOperationWithBody;
 import com.damo.server.domain.schedule.dto.RequestCreateScheduleDto;
 import com.damo.server.domain.schedule.dto.RequestUpdateScheduleDto;
 import com.damo.server.domain.schedule.dto.ScheduleDto;
@@ -9,7 +13,6 @@ import com.damo.server.domain.user.dto.UserDto;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +26,7 @@ public class ScheduleController {
     private final ScheduleReadService scheduleReadServices;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+    @CreateScheduleOperationWithBody(summary = "일정 추가")
     public void addSchedule(
             @Valid @RequestBody final RequestCreateScheduleDto scheduleDto,
             @AuthenticationPrincipal final UserDto user
@@ -31,8 +34,8 @@ public class ScheduleController {
         scheduleWriteService.addSchedule(scheduleDto, user.getId());
     }
 
-    // swagger 추가 예정
     @GetMapping("{scheduleId}")
+    @ScheduleOperationWithBody(summary = "일정 단건 조회")
     public ResponseEntity<ScheduleDto> readSchedule(
             @PathVariable("scheduleId") final Long scheduleId,
             @AuthenticationPrincipal final UserDto user
@@ -43,6 +46,7 @@ public class ScheduleController {
 
     // swagger 추가 예정
     @PatchMapping("{scheduleId}")
+    @UpdateScheduleOperationWithBody(summary = "일정 수정", description = "내역 생략 가능")
     public void patchScheduleById(
             @PathVariable("scheduleId") final Long scheduleId,
             @Valid @RequestBody final RequestUpdateScheduleDto scheduleDto,
@@ -50,8 +54,9 @@ public class ScheduleController {
     ) {
         scheduleWriteService.patchScheduleById(scheduleDto, scheduleId, user.getId());
     }
-  
+
     @DeleteMapping("{scheduleId}")
+    @ScheduleOperationWithNoBody(summary = "일정 삭제")
     public void removeScheduleById(
             @PathVariable("scheduleId") final Long scheduleId,
             @AuthenticationPrincipal final UserDto user
