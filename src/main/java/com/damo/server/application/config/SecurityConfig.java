fbfcea9c874b26f4755da78c5e14d569308c44a5
hoonloper <yonghoonj98@gmail.com>
2024-01-21 19:42:30 +0900

    Setting: Cors 설정 추가

--- src/main/java/com/damo/server/application/config/SecurityConfig.java
+++ src/main/java/com/damo/server/application/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.damo.server.application.config.oauth.OAuth2SuccessHandler;
 import com.damo.server.application.config.oauth.OAuth2UserService;
 import com.damo.server.application.config.oauth.jwt.JwtTokenService;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -24,6 +25,7 @@ public class SecurityConfig {
     private final OAuth2UserService oAuth2UserService;
     private final OAuth2SuccessHandler successHandler;
     private final JwtTokenService jwtTokenService;
+    private final CorsFilter corsFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -44,6 +46,7 @@ public class SecurityConfig {
                         .userInfoEndpoint(endPoint -> endPoint.userService(oAuth2UserService))
                 )
                 .addFilterBefore(new JwtTokenFilter(jwtTokenService), UsernamePasswordAuthenticationFilter.class)
+                .addFilter(corsFilter)
                 .build();
     }
 }
