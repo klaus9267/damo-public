d486de5d9ba17657f0f4a8541b9d518b177dfe10
hoonloper <yonghoonj98@gmail.com>
2024-01-22 21:54:34 +0900

    Feat: SecurityUserUtil 구현

--- src/main/java/com/damo/server/domain/person/service/PersonWriteService.java
+++ src/main/java/com/damo/server/domain/person/service/PersonWriteService.java
@@ -1,5 +1,6 @@
 package com.damo.server.domain.person.service;
 
+import com.damo.server.application.config.user_details.SecurityUserUtil;
 import com.damo.server.application.handler.exception.CustomErrorCode;
 import com.damo.server.application.handler.exception.CustomException;
 import com.damo.server.domain.person.dto.RequestCreatePersonDto;
@@ -15,26 +16,27 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class PersonWriteService {
     private final PersonRepository personRepository;
+    private final SecurityUserUtil securityUserUtil;
 
     @Transactional
-    public void addPerson(final RequestCreatePersonDto personDto, final Long userId) {
+    public void addPerson(final RequestCreatePersonDto personDto) {
         // TODO: 동명이인일 경우 어떻게 해결할 것인가?
-        if(personRepository.existsByNameAndRelationAndUserId(personDto.name(), personDto.relation(), userId)) {
+        if(personRepository.existsByNameAndRelationAndUserId(personDto.name(), personDto.relation(), securityUserUtil.getId())) {
             throw new CustomException(CustomErrorCode.BAD_REQUEST, "관계 내에서 동일한 이름이 존재");
         };
 
-        personRepository.save(Person.toPersonFromRequest(personDto, userId));
+        personRepository.save(Person.toPersonFromRequest(personDto, securityUserUtil.getId()));
     }
 
     @Transactional
-    public void patchPersonById(final RequestUpdatePersonDto personDto, final Long personId, final Long userId) {
-        final Person person = personRepository.findByIdAndUserId(personId, userId).orElseThrow(() -> new CustomException(CustomErrorCode.NOT_FOUND, "수정할 대상을 찾을 수 없음"));
+    public void patchPersonById(final RequestUpdatePersonDto personDto, final Long personId) {
+        final Person person = personRepository.findByIdAndUserId(personId, securityUserUtil.getId()).orElseThrow(() -> new CustomException(CustomErrorCode.NOT_FOUND, "수정할 대상을 찾을 수 없음"));
         person.changeInfo(personDto);
     }
 
     @Transactional
-    public void removePersonById(final Long personId, final Long userId) {
-        personRepository.findByIdAndUserId(personId, userId).orElseThrow(() -> new CustomException(CustomErrorCode.NOT_FOUND, "삭제할 대상을 찾을 수 없음"));
-        personRepository.deleteByIdAndUserId(personId, userId);
+    public void removePersonById(final Long personId) {
+        personRepository.findByIdAndUserId(personId, securityUserUtil.getId()).orElseThrow(() -> new CustomException(CustomErrorCode.NOT_FOUND, "삭제할 대상을 찾을 수 없음"));
+        personRepository.deleteByIdAndUserId(personId, securityUserUtil.getId());
     }
 }
