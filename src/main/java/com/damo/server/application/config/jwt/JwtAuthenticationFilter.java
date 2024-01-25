f960a87452217fb0ed8089fdd13c42ba9566f648
hoonloper <yonghoonj98@gmail.com>
2024-01-25 19:54:37 +0900

    Refactor: 코드 컨벤션, 들여쓰기, 주석 추가 등 코드 정리

--- src/main/java/com/damo/server/application/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/damo/server/application/config/jwt/JwtAuthenticationFilter.java
@@ -4,33 +4,36 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-
+/**
+ * request 요청에서 Authorization 헤더를 추출해 토큰을 검증하는 로직.
+ */
 @RequiredArgsConstructor
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private final JwtTokenService jwtTokenService;
+  private final JwtTokenService jwtTokenService;
 
-    @Override
-    protected void doFilterInternal(
-            final HttpServletRequest request,
-            final HttpServletResponse response,
-            final FilterChain filterChain
-    ) throws ServletException, IOException {
-        final String authorization = request.getHeader("Authorization");
-        final String token = jwtTokenService.resolveToken(authorization);
+  @Override
+  protected void doFilterInternal(
+      @NonNull final HttpServletRequest request,
+      @NonNull final HttpServletResponse response,
+      @NonNull final FilterChain filterChain
+  ) throws ServletException, IOException {
+    final String authorization = request.getHeader("Authorization");
+    final String token = jwtTokenService.resolveToken(authorization);
 
-        if(token != null && jwtTokenService.validateToken(token)) {
-            final Authentication authentication = jwtTokenService.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
-
-        filterChain.doFilter(request, response);
+    if (token != null && jwtTokenService.validateToken(token)) {
+      final Authentication authentication = jwtTokenService.getAuthentication(token);
+      SecurityContextHolder.getContext().setAuthentication(authentication);
     }
+
+    filterChain.doFilter(request, response);
+  }
 }
--- src/main/java/com/damo/server/application/config/jwt/JwtToken.java
+++ src/main/java/com/damo/server/application/config/jwt/JwtToken.java
@@ -2,5 +2,8 @@ package com.damo.server.application.config.jwt;
 
 import lombok.Builder;
 
+/**
+ * JWT 토큰의 액세스 토큰과 리프레시 토큰을 나타냅니다.
+ */
 @Builder
-public record JwtToken (String accessToken, String refreshToken) {}
+public record JwtToken(String accessToken, String refreshToken) {}
