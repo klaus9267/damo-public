1b54ef7571f8e6a39b286c7f0939088e8095c472
Kim minho <klaus9267@gmail.com>
2024-01-26 18:33:41 +0900

    Feat: Controller에서 사용하는 파라미터 Validation 추가

--- src/main/java/com/damo/server/application/handler/CustomExceptionHandler.java
+++ src/main/java/com/damo/server/application/handler/CustomExceptionHandler.java
@@ -1,18 +1,23 @@
 package com.damo.server.application.handler;
 
 
-import com.damo.server.application.handler.exception.*;
+import com.damo.server.application.handler.exception.CustomErrorCode;
+import com.damo.server.application.handler.exception.CustomException;
+import com.damo.server.application.handler.exception.ResponseCustomException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 @Slf4j
 @RestControllerAdvice
 public class CustomExceptionHandler {
-    /** custom 예외 핸들러 */
+    /**
+     * custom 예외 핸들러
+     */
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ResponseCustomException> handleException(final CustomException exception, final HttpServletRequest request) {
         log.error("[code] {}, [url] {}, [message] {}", exception.getCustomErrorCode(), request.getRequestURI(), exception.getMessage());
@@ -20,12 +25,26 @@ public class CustomExceptionHandler {
         return ResponseEntity.status(exception.getCustomErrorCode().getStatusCode()).body(ResponseCustomException.of(exception));
     }
 
-    /** validation 예외 핸들러 */
+    /**
+     * validation 예외 핸들러
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ResponseCustomException> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         log.error("[MethodArgumentNotValidException] " + exception.getMessage());
 
         final CustomException customException = new CustomException(CustomErrorCode.BAD_REQUEST, exception.getMessage());
+        return ResponseEntity.status(customException.getCustomErrorCode().getStatusCode()).body(ResponseCustomException.of(customException));
+    }
+
+    /**
+     * parameter validation 예외 핸들러
+     */
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    protected ResponseEntity<ResponseCustomException> handleHandlerMethodValidationException(HandlerMethodValidationException exception) {
+        final String message = exception.getAllValidationResults().get(0).getResolvableErrors().get(0).getDefaultMessage();
+        log.error("[HandlerMethodValidationException] " + message);
+
+        final CustomException customException = new CustomException(CustomErrorCode.BAD_REQUEST, message);
         return ResponseEntity.status(customException.getCustomErrorCode().getStatusCode()).body(ResponseCustomException.of(customException));
     }
 }
