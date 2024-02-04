b9086303936f16539d207674ccc2b0b4e30c2165
hoonloper <yonghoonj98@gmail.com>
2024-02-04 20:34:45 +0900

    Refactor: OAuthUserClientComposite 코드 스타일 수정

--- src/main/java/com/damo/server/application/config/oauth/OAuthUserClientComposite.java
+++ src/main/java/com/damo/server/application/config/oauth/OAuthUserClientComposite.java
@@ -1,31 +1,35 @@
 package com.damo.server.application.config.oauth;
 
+import com.damo.server.application.config.oauth.provider.OAuthProviderType;
 import com.damo.server.application.handler.exception.CustomErrorCode;
 import com.damo.server.application.handler.exception.CustomException;
-import com.damo.server.application.config.oauth.provider.OAuthProviderType;
 import com.damo.server.domain.user.entity.User;
-import org.springframework.stereotype.Component;
-
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
 
+/**
+ * {@code OAuthUserClientComposite} 클래스는 다양한 OAuth 공급자에 대한 클라이언트들을 관리하고,
+ * 해당 공급자에 따라 사용자 정보를 가져오기 위한 합성 클래스입니다.
+ */
+@SuppressWarnings({"checkstyle:AbbreviationAsWordInName", "checkstyle:ParameterName"})
 @Component
 public class OAuthUserClientComposite {
-    private final Map<OAuthProviderType, OAuthUserClient> map;
+  private final Map<OAuthProviderType, OAuthUserClient> map;
 
-    public OAuthUserClientComposite(final Set<OAuthUserClient> clients) {
-        this.map = clients.stream().collect(Collectors.toMap(OAuthUserClient::providerType, Function.identity()));
-    }
+  public OAuthUserClientComposite(final Set<OAuthUserClient> clients) {
+    this.map = clients.stream().collect(Collectors.toMap(OAuthUserClient::providerType, Function.identity()));
+  }
 
-    public User fetch(final OAuthProviderType oAuthProviderType, final String authCode, final boolean isDev) {
-        return getClient(oAuthProviderType).fetch(authCode, isDev);
-    }
+  public User fetch(final OAuthProviderType oAuthProviderType, final String authCode, final boolean isDev) {
+    return getClient(oAuthProviderType).fetch(authCode, isDev);
+  }
 
-    private OAuthUserClient getClient(OAuthProviderType oAuthProviderType) {
-        return Optional.ofNullable(map.get(oAuthProviderType))
-                .orElseThrow(() -> new CustomException(CustomErrorCode.UNAUTHORIZED, "지원하지 않는 소셜 로그인"));
-    }
+  private OAuthUserClient getClient(OAuthProviderType oAuthProviderType) {
+    return Optional.ofNullable(map.get(oAuthProviderType))
+        .orElseThrow(() -> new CustomException(CustomErrorCode.UNAUTHORIZED, "지원하지 않는 소셜 로그인"));
+  }
 }
