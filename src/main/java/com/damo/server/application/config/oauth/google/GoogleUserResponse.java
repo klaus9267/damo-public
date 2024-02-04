d354c0ed9ca610a049b8abfb6dd0fac49ca0a418
hoonloper <yonghoonj98@gmail.com>
2024-02-04 20:12:18 +0900

    Refactor: GoogleUserResponse에 주석 추가

--- src/main/java/com/damo/server/application/config/oauth/google/GoogleUserResponse.java
+++ src/main/java/com/damo/server/application/config/oauth/google/GoogleUserResponse.java
@@ -6,6 +6,10 @@ import com.damo.server.domain.user.entity.User;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+/**
+ * Google OAuth에서 받은 사용자 정보를 담는 Immutable(불변) 레코드 클래스입니다.
+ * {@code JsonNaming} 애너테이션을 통해 JSON 직렬화 시에 속성 이름을 Snake Case로 변환합니다.
+ */
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record GoogleUserResponse(
     String id,
@@ -17,6 +21,9 @@ public record GoogleUserResponse(
     String picture,
     String locale
 ) {
+  /**
+   * Google 사용자 정보를 도메인 객체로 변환하여 반환하는 메서드입니다.
+   */
   public User toDomain() {
     return User.builder()
       .name(name)
