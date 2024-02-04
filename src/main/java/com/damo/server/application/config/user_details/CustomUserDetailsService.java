edd54be159d604eb93f0a39ced2f9a6f64812bc7
hoonloper <yonghoonj98@gmail.com>
2024-02-04 20:39:54 +0900

    Refactor: CustomUserDetailsService 코드 스타일 수정

--- src/main/java/com/damo/server/application/config/user_details/CustomUserDetailsService.java
+++ src/main/java/com/damo/server/application/config/user_details/CustomUserDetailsService.java
@@ -4,31 +4,44 @@ import com.damo.server.application.handler.exception.CustomErrorCode;
 import com.damo.server.application.handler.exception.CustomException;
 import com.damo.server.domain.user.entity.User;
 import com.damo.server.domain.user.repository.UserRepository;
+import java.util.Collection;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Collections;
-
+/**
+ * {@code CustomUserDetailsService} 클래스는 Spring Security의 {@code UserDetailsService}를
+ * 구현한 사용자 정의 서비스 클래스입니다.
+ * 이 클래스는 {@code UserRepository}를 통해 사용자 정보를 로드하고,
+ * {@code CustomUserDetails} 객체로 변환하여 반환합니다.
+ */
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-    private final UserRepository userRepository;
+  private final UserRepository userRepository;
 
-    @Override
-    public CustomUserDetails loadUserByUsername(final String username) {
-        return userRepository.findOneByUsername(username)
-                .map(this::createUserDetails)
-                .orElseThrow(() -> new CustomException(CustomErrorCode.UNAUTHORIZED, "유저 정보를 찾을 수 없습니다."));
-    }
+  @Override
+  public CustomUserDetails loadUserByUsername(final String username) {
+    return userRepository.findOneByUsername(username)
+        .map(this::createUserDetails)
+        .orElseThrow(() -> new CustomException(CustomErrorCode.UNAUTHORIZED, "유저 정보를 찾을 수 없습니다."));
+  }
 
-    private CustomUserDetails createUserDetails(final User user) {
-        final Collection<? extends GrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority(user.getRole().getKey()));
+  private CustomUserDetails createUserDetails(final User user) {
+    final Collection<? extends GrantedAuthority> authorities = Collections.singleton(
+      new SimpleGrantedAuthority(user.getRole().getKey())
+    );
 
-        return new CustomUserDetails(user.getUsername(), user.getProviderId(), authorities, user.getId(), user.getName());
-    }
+    return new CustomUserDetails(
+        user.getUsername(),
+        user.getProviderId(),
+        authorities,
+        user.getId(),
+        user.getName()
+    );
+  }
 }
 
