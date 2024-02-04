e28c8b6058aa0486bd570c31b755b8c8e22f41f5
hoonloper <yonghoonj98@gmail.com>
2024-02-04 20:32:36 +0900

    Refactor: OAuthProvierTypeConverter 코드 스타일 수정

--- src/main/java/com/damo/server/application/config/oauth/OAuthProviderTypeConverter.java
+++ src/main/java/com/damo/server/application/config/oauth/OAuthProviderTypeConverter.java
@@ -1,12 +1,16 @@
 package com.damo.server.application.config.oauth;
 
-
 import com.damo.server.application.config.oauth.provider.OAuthProviderType;
+import lombok.NonNull;
 import org.springframework.core.convert.converter.Converter;
 
+/**
+ * {@code OAuthProviderTypeConverter} 클래스는 문자열을 {@code OAuthProviderType}으로 변환하는 역할을 합니다.
+ */
+@SuppressWarnings("checkstyle:AbbreviationAsWordInName")
 public class OAuthProviderTypeConverter implements Converter<String, OAuthProviderType> {
-    @Override
-    public OAuthProviderType convert(final String source) {
-        return OAuthProviderType.from(source);
-    }
+  @Override
+  public OAuthProviderType convert(@NonNull final String source) {
+    return OAuthProviderType.from(source);
+  }
 }
