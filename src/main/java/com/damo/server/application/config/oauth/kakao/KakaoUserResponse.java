6f3c10e53a6a1d4690f7008521c14ec4a6ddd463
hoonloper <yonghoonj98@gmail.com>
2024-02-04 20:17:54 +0900

    Refactor: KakaoUserResponse 코드 스타일 수정

--- src/main/java/com/damo/server/application/config/oauth/kakao/KakaoUserResponse.java
+++ src/main/java/com/damo/server/application/config/oauth/kakao/KakaoUserResponse.java
@@ -7,6 +7,10 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import java.time.LocalDateTime;
 
+/**
+ * Kakao OAuth에서 받은 사용자 정보를 담는 Immutable(불변) 레코드 클래스입니다.
+ * {@code JsonNaming} 애너테이션을 통해 JSON 직렬화 시에 속성 이름을 Snake Case로 변환합니다.
+ */
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoUserResponse(
     Long id,
@@ -14,7 +18,11 @@ public record KakaoUserResponse(
     LocalDateTime connectedAt,
     KakaoAccount kakaoAccount
 ) {
-
+  /**
+   * 해당 Kakao 사용자 정보를 도메인 객체로 변환하여 반환하는 메서드입니다.
+   *
+   * @return Kakao 사용자 정보를 담은 {@link User} 도메인 객체
+   */
   public User toDomain() {
     return User.builder()
       .name(kakaoAccount.profile.nickname) // TODO: 이름은 스코프에 없음
@@ -27,6 +35,9 @@ public record KakaoUserResponse(
       .build();
   }
 
+  /**
+   * Kakao 사용자의 계정 정보를 담는 Immutable(불변) 레코드 클래스입니다.
+   */
   @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
   public record KakaoAccount(
       boolean profileNeedsAgreement,
@@ -56,6 +67,9 @@ public record KakaoUserResponse(
   ) {
   }
 
+  /**
+   * Kakao 사용자의 프로필 정보를 담는 Immutable(불변) 레코드 클래스입니다.
+   */
   @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
   public record Profile(
       String nickname,
