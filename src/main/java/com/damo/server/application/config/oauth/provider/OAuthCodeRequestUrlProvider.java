3d8c17cefbd69ec7e59ab856fd80bb1224266cd0
hoonloper <yonghoonj98@gmail.com>
2024-02-04 20:27:31 +0900

    Refactor: OAuthCodeRequestUrlProvider 코드 스타일 수정

--- src/main/java/com/damo/server/application/config/oauth/provider/OAuthCodeRequestUrlProvider.java
+++ src/main/java/com/damo/server/application/config/oauth/provider/OAuthCodeRequestUrlProvider.java
@@ -1,6 +1,13 @@
 package com.damo.server.application.config.oauth.provider;
 
+/**
+ * {@code OAuthCodeRequestUrlProvider} 인터페이스는 OAuth 코드 요청 URL을 제공하는 역할을 정의합니다.
+ * 구현체들은 각각의 OAuth 공급자에 특화된 코드 요청 URL을 생성하는 역할을 수행합니다.
+ * {@code checkstyle:AbbreviationAsWordInName} 경고를 무시하는 애너테이션을 사용하였습니다.
+ */
+@SuppressWarnings("checkstyle:AbbreviationAsWordInName")
 public interface OAuthCodeRequestUrlProvider {
-    OAuthProviderType providerType();
-    String provide(final boolean isDev);
+  OAuthProviderType providerType();
+
+  String provide(final boolean isDev);
 }
