85963993a60c74a6af278d945a50496693318663
hoonloper <yonghoonj98@gmail.com>
2024-02-04 20:40:35 +0900

    Refactor: SecurityUserUtil 코드 스타일 수정

--- src/main/java/com/damo/server/application/config/user_details/SecurityUserUtil.java
+++ src/main/java/com/damo/server/application/config/user_details/SecurityUserUtil.java
@@ -6,27 +6,31 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+/**
+ * {@code SecurityUserUtil} 클래스는 Spring Security의 보안 컨텍스트에서 현재 인증된 사용자의 정보를
+ * 추출하여 제공하는 유틸리티 클래스입니다.
+ */
 @Component
 public class SecurityUserUtil {
-    public Long getId() {
-        return getCustomUserDetails().getId();
+  public Long getId() {
+    return getCustomUserDetails().getId();
+  }
+
+  public String getName() {
+    return getCustomUserDetails().getName();
+  }
+
+  public String getUsername() {
+    return getCustomUserDetails().getUsername();
+  }
+
+  private CustomUserDetails getCustomUserDetails() {
+    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication == null || authentication.getName() == null) {
+      throw new CustomException(CustomErrorCode.UNAUTHORIZED, "유저 정보를 찾을 수 없습니다.");
     }
 
-    public String getName() {
-        return getCustomUserDetails().getName();
-    }
-
-    public String getUsername() {
-        return getCustomUserDetails().getUsername();
-    }
-
-    private CustomUserDetails getCustomUserDetails() {
-        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || authentication.getName() == null) {
-            throw new CustomException(CustomErrorCode.UNAUTHORIZED, "유저 정보를 찾을 수 없습니다.");
-        }
-
-        return (CustomUserDetails) authentication.getPrincipal();
-    }
+    return (CustomUserDetails) authentication.getPrincipal();
+  }
 }
 
