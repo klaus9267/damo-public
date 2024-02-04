2fb6b31a7d4987c757bc0ec50e21d59047674db6
hoonloper <yonghoonj98@gmail.com>
2024-02-04 20:24:26 +0900

    Refactor: GoogleOAuthCodeRequestUrlProvider 코드 스타일 수정

--- src/main/java/com/damo/server/application/config/oauth/provider/GoogleOAuthCodeRequestUrlProvider.java
+++ src/main/java/com/damo/server/application/config/oauth/provider/GoogleOAuthCodeRequestUrlProvider.java
@@ -6,32 +6,43 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
+/**
+ * {@code GoogleOAuthCodeRequestUrlProvider} 클래스는 Google OAuth의 코드 요청 URL을 제공하는 컴포넌트입니다.
+ * {@code OAuthCodeRequestUrlProvider} 인터페이스를 구현하며, Google OAuth에 특화된 설정을 이용하여
+ * 코드 요청 URL을 생성합니다.
+ *
+ * {@code checkstyle:AbbreviationAsWordInName} 경고를 무시하는 애너테이션을 사용하였습니다.
+ */
+@SuppressWarnings("checkstyle:AbbreviationAsWordInName")
 @Component
 @RequiredArgsConstructor
 public class GoogleOAuthCodeRequestUrlProvider implements OAuthCodeRequestUrlProvider {
-    private final GoogleOAuthConfig googleOAuthConfig;
+  private final GoogleOAuthConfig googleOAuthConfig;
 
-    @Value("${oauth.is-back}")
-    private boolean isBack;
+  @Value("${oauth.is-back}")
+  private boolean isBack;
 
-    @Override
-    public OAuthProviderType providerType() {
-        return OAuthProviderType.GOOGLE;
+  @Override
+  public OAuthProviderType providerType() {
+    return OAuthProviderType.GOOGLE;
+  }
+
+  @Override
+  public String provide(final boolean isDev) {
+    String redirectUri;
+    if (isBack) {
+      redirectUri = googleOAuthConfig.backRedirectUri();
+    } else if (isDev) {
+      redirectUri = googleOAuthConfig.devRedirectUri();
+    } else {
+      redirectUri = googleOAuthConfig.redirectUri();
     }
-
-    @Override
-    public String provide(final boolean isDev) {
-        final String redirectUri = isBack
-                ? googleOAuthConfig.backRedirectUri()
-                : isDev
-                ? googleOAuthConfig.devRedirectUri()
-                : googleOAuthConfig.redirectUri();
-        return UriComponentsBuilder
-                .fromUriString("https://accounts.google.com/o/oauth2/auth")
-                .queryParam("response_type", "code")
-                .queryParam("client_id", googleOAuthConfig.clientId())
-                .queryParam("redirect_uri", redirectUri)
-                .queryParam("scope", String.join(" ", googleOAuthConfig.scope()))
-                .toUriString();
-    }
+    return UriComponentsBuilder
+        .fromUriString("https://accounts.google.com/o/oauth2/auth")
+        .queryParam("response_type", "code")
+        .queryParam("client_id", googleOAuthConfig.clientId())
+        .queryParam("redirect_uri", redirectUri)
+        .queryParam("scope", String.join(" ", googleOAuthConfig.scope()))
+        .toUriString();
+  }
 }
