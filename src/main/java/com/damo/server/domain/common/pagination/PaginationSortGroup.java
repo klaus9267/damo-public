9d064c7c98987850faa45bc92b0470121322e340
hoonloper <yonghoonj98@gmail.com>
2024-02-05 19:37:03 +0900

    Refactor: Pagination 관련 클래스 코드 스타일 수정

--- src/main/java/com/damo/server/domain/common/pagination/PaginationSortGroup.java
+++ src/main/java/com/damo/server/domain/common/pagination/PaginationSortGroup.java
@@ -1,17 +1,23 @@
 package com.damo.server.domain.common.pagination;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.List;
-
+/**
+ * {@code PaginationSortGroup}은 페이지네이션에서 사용되는 정렬 그룹을 정의하는 열거형입니다.
+ * 각 정렬 그룹에는 허용된 정렬 유형 목록이 포함되어 있습니다.
+ */
 @AllArgsConstructor
 @Getter
 public enum PaginationSortGroup {
-    PERSON(List.of(PaginationSortType.NAME, PaginationSortType.RELATION, PaginationSortType.CREATED_AT)),
-    TRANSACTION(List.of(PaginationSortType.ACTION, PaginationSortType.SCHEDULE_EVENT_DATE)),
-    SCHEDULE(List.of(PaginationSortType.EVENT_DATE)),
-    EMPTY(List.of(PaginationSortType.EMPTY));
+  PERSON(List.of(PaginationSortType.NAME, PaginationSortType.RELATION, PaginationSortType.CREATED_AT)),
+  TRANSACTION(List.of(PaginationSortType.ACTION, PaginationSortType.SCHEDULE_EVENT_DATE)),
+  SCHEDULE(List.of(PaginationSortType.EVENT_DATE)),
+  EMPTY(List.of(PaginationSortType.EMPTY));
 
-    private final List<PaginationSortType> sortTypes;
+  /**
+   * 정렬 그룹에 속하는 허용된 정렬 유형 목록.
+   */
+  private final List<PaginationSortType> sortTypes;
 }
