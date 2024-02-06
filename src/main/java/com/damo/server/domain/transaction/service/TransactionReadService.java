b7384ada40dc1a4e1b7a46c0b3ff87979cd05409
Kim minho <klaus9267@gmail.com>
2024-02-06 21:16:55 +0900

    Refactor: service 코드 스타일 수정

--- src/main/java/com/damo/server/domain/transaction/service/TransactionReadService.java
+++ src/main/java/com/damo/server/domain/transaction/service/TransactionReadService.java
@@ -5,7 +5,7 @@ import com.damo.server.application.handler.exception.CustomErrorCode;
 import com.damo.server.application.handler.exception.CustomException;
 import com.damo.server.domain.common.pagination.param.TransactionPaginationParam;
 import com.damo.server.domain.transaction.TransactionRepository;
-import com.damo.server.domain.transaction.TransactionTotalAmount;
+import com.damo.server.domain.transaction.dto.TransactionTotalAmountDto;
 import com.damo.server.domain.transaction.dto.TransactionPaginationResponseDto;
 import com.damo.server.domain.transaction.dto.TransactionWithScheduleDto;
 import com.damo.server.domain.transaction.entity.Transaction;
@@ -15,29 +15,54 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
-
+/**
+ * `TransactionReadService`는 주어진 조건에 따라 거래 내역 및 거래 총액을 조회하는 서비스를 제공합니다.
+ */
 @AllArgsConstructor
 @Service
 public class TransactionReadService {
-    private final TransactionRepository transactionRepository;
-    private final SecurityUserUtil securityUserUtil;
-
-    public TransactionTotalAmount readTotalAmounts() {
-        return transactionRepository.findTotalAmount(securityUserUtil.getId());
-    }
-
-    public TransactionTotalAmount readRecentAmounts(final LocalDateTime startedAt) {
-        final LocalDateTime startDate = startedAt == null ? LocalDateTime.now().minusMonths(1) : startedAt;
-        return transactionRepository.readRecentAmounts(securityUserUtil.getId(), startDate);
-    }
-
-    public TransactionWithScheduleDto readTransaction(final Long transactionId) {
-        final Transaction transaction = transactionRepository.findByIdAndUserId(transactionId, securityUserUtil.getId()).orElseThrow(() -> new CustomException(CustomErrorCode.NOT_FOUND, "조회할 거래 내역을 찾을 수 없음"));
-        return TransactionWithScheduleDto.from(transaction);
-    }
-
-    public TransactionPaginationResponseDto readTransactionList(final TransactionPaginationParam param) {
-        final Page<TransactionWithScheduleDto> transactionPage = transactionRepository.findAllByUserId(param.toPageable(), securityUserUtil.getId(), param.getStartedAt(), param.getEndedAt(), param.getAction());
-        return TransactionPaginationResponseDto.from(transactionPage);
-    }
+  private final TransactionRepository transactionRepository;
+  private final SecurityUserUtil securityUserUtil;
+  
+  /**
+   * 사용자 ID에 따라 거래 총액을 조회 후 결과를 반환합니다.
+   *
+   * @return 조회된 거래 총액
+   */
+  public TransactionTotalAmountDto readTotalAmounts() {
+    return transactionRepository.findTotalAmount(securityUserUtil.getId());
+  }
+  
+  /**
+   * 사용자 ID와 조회 시작 날짜에 따라 거래 총액을 조회 후 결과를 반환합니다.
+   *
+   * @param startedAt 조회 시작 날짜
+   * @return 조회딘 거래 총액
+   */
+  public TransactionTotalAmountDto readRecentAmounts(final LocalDateTime startedAt) {
+    final LocalDateTime startDate = startedAt == null ? LocalDateTime.now().minusMonths(1) : startedAt;
+    return transactionRepository.readRecentAmounts(securityUserUtil.getId(), startDate);
+  }
+  
+  /**
+   * 사용자 ID와 거래 내역 ID에 따라 내역을 조회하고, 조회 결과를 응답 DTO로 반홥합니다.
+   *
+   * @param transactionId 조회할 내역 ID
+   * @return 조회된 내역, 일정을 포함하는 내역 DTO
+   */
+  public TransactionWithScheduleDto readTransaction(final Long transactionId) {
+    final Transaction transaction = transactionRepository.findByIdAndUserId(transactionId, securityUserUtil.getId()).orElseThrow(() -> new CustomException(CustomErrorCode.NOT_FOUND, "조회할 거래 내역을 찾을 수 없음"));
+    return TransactionWithScheduleDto.from(transaction);
+  }
+  
+  /**
+   * 사용자 ID, 조회 시작 날짜, 조회 종료 날짜, 거래 종류에 따라 내역을 조회하고, 조회 결롸를 페이지로 반환합니다.
+   *
+   * @param param 조회할 내역 목록의 페이징 및 검색 조건
+   * @return 조회된 내역 및 일정 정보를 포함하는 페이지 응답 DTO
+   */
+  public TransactionPaginationResponseDto readTransactionList(final TransactionPaginationParam param) {
+    final Page<TransactionWithScheduleDto> transactionPage = transactionRepository.findAllByUserId(param.toPageable(), securityUserUtil.getId(), param.getStartedAt(), param.getEndedAt(), param.getAction());
+    return TransactionPaginationResponseDto.from(transactionPage);
+  }
 }
--- src/main/java/com/damo/server/domain/transaction/service/TransactionWriteService.java
+++ src/main/java/com/damo/server/domain/transaction/service/TransactionWriteService.java
@@ -11,29 +11,48 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * * `TransactionWriteService`는 내역을 추가, 수정, 삭제하는 서비스를 제공합니다.
+ */
 @AllArgsConstructor
 @Service
 public class TransactionWriteService {
-    private final TransactionRepository transactionRepository;
-    private final SecurityUserUtil securityUserUtil;
-
-    @Transactional
-    public void save(final RequestCreateTransactionDto transactionDto) {
-        if (transactionRepository.existsByEventDateAndPersonIdAndEvent(transactionDto.eventDate(), transactionDto.personId(), transactionDto.event())) {
-            throw new CustomException(CustomErrorCode.BAD_REQUEST, "내역에서 동일한 기록이 존재");
-        }
-        transactionRepository.save(Transaction.from(transactionDto, securityUserUtil.getId()));
+  private final TransactionRepository transactionRepository;
+  private final SecurityUserUtil securityUserUtil;
+  
+  /**
+   * 새로운 내역, 일정을 추가합니다
+   *
+   * @param transactionDto 추가할 내역 정보를 담은 DTO
+   */
+  @Transactional
+  public void save(final RequestCreateTransactionDto transactionDto) {
+    if (transactionRepository.existsByEventDateAndPersonIdAndEvent(transactionDto.eventDate(), transactionDto.personId(), transactionDto.event())) {
+      throw new CustomException(CustomErrorCode.BAD_REQUEST, "내역에서 동일한 기록이 존재");
     }
-
-    @Transactional
-    public void patchTransactionById(final RequestUpdateTransactionDto transactionDto, final Long transactionId) {
-        final Transaction transaction = transactionRepository.findByIdAndUserId(transactionId, securityUserUtil.getId()).orElseThrow(() -> new CustomException(CustomErrorCode.NOT_FOUND, "수정할 내역을 찾을 수 없음"));
-        transaction.changeInfo(transactionDto);
-    }
-
-    @Transactional
-    public void removeTransactionById(final Long transactionId) {
-        transactionRepository.findByIdAndUserId(transactionId, securityUserUtil.getId()).orElseThrow(() -> new CustomException(CustomErrorCode.NOT_FOUND, "삭제할 내역을 찾을 수 없음"));
-        transactionRepository.deleteById(transactionId);
-    }
+    transactionRepository.save(Transaction.from(transactionDto, securityUserUtil.getId()));
+  }
+  
+  /**
+   * 지정된 내역 ID에 해당하는 내역을 수정합니다.
+   *
+   * @param transactionDto 수정할 내역 정보를 담은 DTO
+   * @param transactionId  수정할 내역의 ID
+   */
+  @Transactional
+  public void patchTransactionById(final RequestUpdateTransactionDto transactionDto, final Long transactionId) {
+    final Transaction transaction = transactionRepository.findByIdAndUserId(transactionId, securityUserUtil.getId()).orElseThrow(() -> new CustomException(CustomErrorCode.NOT_FOUND, "수정할 내역을 찾을 수 없음"));
+    transaction.changeInfo(transactionDto);
+  }
+  
+  /**
+   * 지정된 내역 ID에 해당하는 내역을 삭제합니다.
+   *
+   * @param transactionId 삭제할 내역의 ID
+   */
+  @Transactional
+  public void removeTransactionById(final Long transactionId) {
+    transactionRepository.findByIdAndUserId(transactionId, securityUserUtil.getId()).orElseThrow(() -> new CustomException(CustomErrorCode.NOT_FOUND, "삭제할 내역을 찾을 수 없음"));
+    transactionRepository.deleteById(transactionId);
+  }
 }
