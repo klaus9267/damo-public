8ac23eb5b5a70a9444eeef6cf7a10d16d49dd3ba
Kim minho <klaus9267@gmail.com>
2024-02-20 21:22:31 +0900

    TEST: 내역 성공 케이스 test

--- src/test/java/com/damo/server/application/controller/TransactionControllerTest.java
+++ src/test/java/com/damo/server/application/controller/TransactionControllerTest.java
@@ -3,6 +3,7 @@ package com.damo.server.application.controller;
 import com.damo.server.common.WithMockCustomUser;
 import com.damo.server.domain.person.repository.PersonRepository;
 import com.damo.server.domain.transaction.dto.RequestCreateTransactionDto;
+import com.damo.server.domain.transaction.dto.RequestUpdateTransactionDto;
 import com.damo.server.domain.transaction.entity.TransactionAction;
 import com.damo.server.domain.transaction.entity.TransactionAmount;
 import com.damo.server.domain.transaction.entity.TransactionCategory;
@@ -24,7 +25,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -55,12 +56,12 @@ class TransactionControllerTest {
       final TransactionAction action = TransactionAction.RECEIVING;
       final TransactionAmount amount = new TransactionAmount(action, 1000L);
       final TransactionCategory category = TransactionCategory.ETC;
-      final RequestCreateTransactionDto requestCreateTransactionDto = new RequestCreateTransactionDto(1L, now, "테스트 이벤트", amount, category, "테스트 메모");
+      final RequestCreateTransactionDto createTransactionDto = new RequestCreateTransactionDto(1L, now, "테스트 이벤트", amount, category, "테스트 메모");
       
       final ObjectMapper mapper = new ObjectMapper()
           .registerModule(new JavaTimeModule())
           .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-      final String json = mapper.writeValueAsString(requestCreateTransactionDto);
+      final String json = mapper.writeValueAsString(createTransactionDto);
       
       mvc.perform(post(END_POINT)
              .contentType(MediaType.APPLICATION_JSON)
@@ -68,6 +69,34 @@ class TransactionControllerTest {
              .accept(MediaType.APPLICATION_JSON)
          ).andDo(print())
          .andExpect(status().isCreated());
+    }
+    
+    @Test
+    void 내역_수정() throws Exception {
+      final TransactionAction action = TransactionAction.RECEIVING;
+      final TransactionAmount amount = new TransactionAmount(action, 1000L);
+      final TransactionCategory category = TransactionCategory.ETC;
+      final RequestUpdateTransactionDto updateTransactionDto = new RequestUpdateTransactionDto(1L, amount, category, "테스트 메모");
+      final ObjectMapper mapper = new ObjectMapper()
+          .registerModule(new JavaTimeModule())
+          .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+      final String json = mapper.writeValueAsString(updateTransactionDto);
+      
+      mvc.perform(patch(END_POINT + "/" + 1)
+             .contentType(MediaType.APPLICATION_JSON)
+             .content(json)
+             .accept(MediaType.APPLICATION_JSON)
+         ).andDo(print())
+         .andExpect(status().isNoContent());
+    }
+    
+    @Test
+    void 내역_삭제() throws Exception {
+      mvc.perform(delete(END_POINT + "/" + 1L)
+             .contentType(MediaType.APPLICATION_JSON)
+             .accept(MediaType.APPLICATION_JSON)
+         ).andDo(print())
+         .andExpect(status().isNoContent());
     }
   }
 }
