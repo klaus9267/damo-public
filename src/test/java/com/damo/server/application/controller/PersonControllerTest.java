5c3650c12e47d238bdd66dabab87eeb089376de3
hoonloper <yonghoonj98@gmail.com>
2024-02-26 15:41:38 +0900

    Test: 대상 생성 실패 케이스 추가

--- src/test/java/com/damo/server/application/controller/PersonControllerTest.java
+++ src/test/java/com/damo/server/application/controller/PersonControllerTest.java
@@ -34,6 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @Transactional
+@WithMockCustomUser
 public class PersonControllerTest {
     @MockBean
     PersonWriteService personWriteService;
@@ -46,7 +47,6 @@ public class PersonControllerTest {
 
     @Nested
     @DisplayName("성공 케이스")
-    @WithMockCustomUser
     class 성공 {
         LocalDateTime now;
 
@@ -103,78 +103,74 @@ public class PersonControllerTest {
               .andExpect(status().isNoContent());
         }
     }
-//    @Nested
-//    @DisplayName("실패 케이스")
-//    class 실패 {
-//        Timestamp now;
-//        @BeforeEach
-//        void 초기값() {
-//            now = Timestamp.valueOf(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
-//        }
-//
-//
-//        @Test
-//        void 대상_생성_실패() throws Exception {
-//
-//            FailPersonDto personDto = new FailPersonDto(null, "가족", null, 1L);
-//            ObjectMapper mapper = new ObjectMapper()
-//                    .registerModule(new JavaTimeModule())
-//                    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-//            String json = mapper.writeValueAsString(personDto);
-//
-//            // 이름 실패
-//            mvc.perform(post(END_POINT)
-//                            .contentType(MediaType.APPLICATION_JSON)
-//                            .content(json)
-//                    )
-//                    .andExpect(status().isBadRequest())
-//                    .andDo(print());
-//
-//            personDto = new FailPersonDto("이름", null, null, 1L);
-//            mapper = new ObjectMapper()
-//                    .registerModule(new JavaTimeModule())
-//                    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-//            json = mapper.writeValueAsString(personDto);
-//
-//            // 관계 실패
-//            mvc.perform(post(END_POINT)
-//                            .contentType(MediaType.APPLICATION_JSON)
-//                            .content(json)
-//                    )
-//                    .andExpect(status().isBadRequest())
-//                    .andDo(print());
-//
-//
-//            personDto = new FailPersonDto("이름", "가족", 123, 1L);
-//            mapper = new ObjectMapper()
-//                    .registerModule(new JavaTimeModule())
-//                    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-//            json = mapper.writeValueAsString(personDto);
-//
-//            // 메모 실패
-//            mvc.perform(post(END_POINT)
-//                            .contentType(MediaType.APPLICATION_JSON)
-//                            .content(json)
-//                    )
-//                    .andExpect(status().isBadRequest())
-//                    .andDo(print());
-//
-//
-//            personDto = new FailPersonDto("이름", "가족", "112233", 0L);
-//            mapper = new ObjectMapper()
-//                    .registerModule(new JavaTimeModule())
-//                    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-//            json = mapper.writeValueAsString(personDto);
-//
-//            // 유저 아이디 실패
-//            mvc.perform(post(END_POINT)
-//                            .contentType(MediaType.APPLICATION_JSON)
-//                            .content(json)
-//                    )
-//                    .andExpect(status().isBadRequest())
-//                    .andDo(print());
-//        }
-//    }
+    @Nested
+    @DisplayName("실패 케이스")
+    class 실패 {
+        @Nested
+        @DisplayName("대상_생성_실패")
+        class 대상_생성_실패 {
+            private void callApiForBadRequestWhenCreate(final FailPersonDto failPersonDto) throws Exception {
+                final String json = mapper.writeValueAsString(failPersonDto);
+
+                mvc.perform(post(END_POINT)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json)
+                  )
+                  .andExpect(status().isBadRequest())
+                  .andDo(print());
+            }
+            @Test
+            void 대상_이름값_없을_때() throws Exception {
+                final FailPersonDto personDto = new FailPersonDto(null, PersonRelation.FAMILY, "메모", "01012345678");
+                callApiForBadRequestWhenCreate(personDto);
+            }
+            @Test
+            void 대상_이름_빈문자열() throws Exception {
+                final FailPersonDto personDto = new FailPersonDto("", PersonRelation.FAMILY, "메모", "01012345678");
+                callApiForBadRequestWhenCreate(personDto);
+            }
+            @Test
+            void 대상_이름_20자_초과() throws Exception {
+                final FailPersonDto personDto = new FailPersonDto("111111111111111111111", PersonRelation.FAMILY, "메모", "01012345678");
+                callApiForBadRequestWhenCreate(personDto);
+            }
+            @Test
+            void 대상_관계값_없을_때() throws Exception {
+                final FailPersonDto personDto = new FailPersonDto("홍길동", null, "메모", "01012345678");
+                callApiForBadRequestWhenCreate(personDto);
+            }
+            @Test
+            void 대상_관계_빈문자열() throws Exception {
+                final FailPersonDto personDto = new FailPersonDto("홍길동", "", "메모", "01012345678");
+                callApiForBadRequestWhenCreate(personDto);
+            }
+            @Test
+            void 대상_관계_허용되지않은() throws Exception {
+                final FailPersonDto personDto = new FailPersonDto("홍길동", "FASTER", "메모", "01012345678");
+                callApiForBadRequestWhenCreate(personDto);
+            }
+            @Test
+            void 대상_메모_200자_초과() throws Exception {
+                final FailPersonDto personDto = new FailPersonDto("홍길동", PersonRelation.FAMILY, "글".repeat(201), "01012345678");
+                callApiForBadRequestWhenCreate(personDto);
+            }
+            @Test
+            void 대상_연락처_빈문자열() throws Exception {
+                final FailPersonDto personDto = new FailPersonDto("홍길동", PersonRelation.FAMILY, "메모", "");
+                callApiForBadRequestWhenCreate(personDto);
+            }
+            @Test
+            void 대상_연락처_8자() throws Exception {
+                final FailPersonDto personDto = new FailPersonDto("홍길동", PersonRelation.FAMILY, "메모", "0".repeat(8));
+                callApiForBadRequestWhenCreate(personDto);
+            }
+            @Test
+            void 대상_연락처_12자() throws Exception {
+                final FailPersonDto personDto = new FailPersonDto("홍길동", PersonRelation.FAMILY, "메모", "0".repeat(12));
+                callApiForBadRequestWhenCreate(personDto);
+            }
+        }
+    }
 }
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
@@ -182,11 +178,11 @@ class FailPersonDto {
     private final Object name;
     private final Object relation;
     private final Object memo;
-    private final Object userId;
-    public FailPersonDto(Object name, Object relation, Object memo, Object userId) {
+    private final Object contact;
+    public FailPersonDto(final Object name, final Object relation, final Object memo, final Object contact) {
         this.name = name;
         this.relation = relation;
         this.memo = memo;
-        this.userId = userId;
+        this.contact = contact;
     }
 }
