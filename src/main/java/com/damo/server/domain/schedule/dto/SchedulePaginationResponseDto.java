36f2d63206e93e0e1b35195ba59879d9ecbba618
hoonloper <yonghoonj98@gmail.com>
2024-02-28 12:27:36 +0900

    Feat: Schedule 페이지네이션 Dto에도 적용

--- src/main/java/com/damo/server/domain/schedule/dto/SchedulePaginationResponseDto.java
+++ src/main/java/com/damo/server/domain/schedule/dto/SchedulePaginationResponseDto.java
@@ -1,18 +1,22 @@
 package com.damo.server.domain.schedule.dto;
 
+import com.damo.server.domain.common.pagination.dto.PaginationResponseDto;
+import java.util.List;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
-
-import java.util.List;
 
 /**
  * {@code SchedulePaginationResponseDto}는 페이지네이션된 일정 목록의 응답 DTO입니다.
  */
 @Getter
-public class SchedulePaginationResponseDto {
+public class SchedulePaginationResponseDto
+    implements PaginationResponseDto<ScheduleWithTransactionDto> {
   private final Integer totalPages;
   private final Long totalElements;
-  private final List<ScheduleWithTransactionDto> schedules;
+  private final Boolean isFirst;
+  private final Boolean isLast;
+  private final Boolean hasNext;
+  private final List<ScheduleWithTransactionDto> items;
   
   /**
    * 내부 생성자로 페이지 객체를 받아 필드를 초기화합니다.
@@ -20,7 +24,10 @@ public class SchedulePaginationResponseDto {
   private SchedulePaginationResponseDto(final Page<ScheduleWithTransactionDto> schedulePage) {
     this.totalPages = schedulePage.getTotalPages();
     this.totalElements = schedulePage.getTotalElements();
-    this.schedules = schedulePage.getContent();
+    this.isFirst = schedulePage.isFirst();
+    this.isLast = schedulePage.isLast();
+    this.hasNext = schedulePage.hasNext();
+    this.items = schedulePage.getContent();
   }
   
   /**
