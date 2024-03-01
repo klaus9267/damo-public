7ce97cd7f5328e84ae6c5b57f879680fdc804b3c
Kim minho <klaus9267@gmail.com>
2024-03-01 21:02:43 +0900

    Test: 수정사항 반영

--- src/test/java/com/damo/server/application/controller/TransactionControllerTest.java
+++ src/test/java/com/damo/server/application/controller/TransactionControllerTest.java
@@ -5,8 +5,8 @@ import com.damo.server.application.handler.exception.CustomException;
 import com.damo.server.common.WithMockCustomUser;
 import com.damo.server.domain.transaction.dto.RequestCreateTransactionDto;
 import com.damo.server.domain.transaction.dto.RequestUpdateTransactionDto;
+import com.damo.server.domain.transaction.dto.TransactionAmountDto;
 import com.damo.server.domain.transaction.entity.TransactionAction;
-import com.damo.server.domain.transaction.entity.TransactionAmount;
 import com.damo.server.domain.transaction.entity.TransactionCategory;
 import com.damo.server.domain.transaction.service.TransactionWriteService;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
@@ -81,8 +81,8 @@ class TransactionControllerTest {
           if (TransactionAction.TOTAL.equals(transactionAction)) {
             continue;
           }
-          
-          final RequestUpdateTransactionDto updateTransactionDto = new RequestUpdateTransactionDto(1L, transactionAction, 1000L, category, "테스트 메모");
+          final TransactionAmountDto transactionAmount = new TransactionAmountDto(transactionAction, 1000L);
+          final RequestUpdateTransactionDto updateTransactionDto = new RequestUpdateTransactionDto(1L, transactionAmount, category, "테스트 메모");
           
           doNothing().when(transactionWriteService).patchTransactionById(updateTransactionDto, transactionId);
           
@@ -100,8 +100,9 @@ class TransactionControllerTest {
       
       @Test
       void 내역_수정_거래_자산_유형() throws Exception {
+        final TransactionAmountDto transactionAmount = new TransactionAmountDto(action, 1000L);
         for (final TransactionCategory transactionCategory : TransactionCategory.values()) {
-          final RequestUpdateTransactionDto updateTransactionDto = new RequestUpdateTransactionDto(1L, action, 1000L, transactionCategory, "테스트 메모");
+          final RequestUpdateTransactionDto updateTransactionDto = new RequestUpdateTransactionDto(1L, transactionAmount, transactionCategory, "테스트 메모");
           
           doNothing().when(transactionWriteService).patchTransactionById(updateTransactionDto, transactionId);
           
@@ -131,29 +132,31 @@ class TransactionControllerTest {
       @Test
       void 내역_생성_거래_종류() throws Exception {
         for (final TransactionAction transactionAction : TransactionAction.values()) {
-          if (!TransactionAction.TOTAL.equals(transactionAction)) {
-            final TransactionAmount transactionAmount = new TransactionAmount(transactionAction, 1000L);
-            final RequestCreateTransactionDto createTransactionDto = new RequestCreateTransactionDto(1L, now, "테스트 이벤트", transactionAction, 1000L, category, "테스트 메모");
-            
-            doNothing().when(transactionWriteService).save(createTransactionDto);
-            
-            final String json = mapper.writeValueAsString(createTransactionDto);
-            
-            mvc.perform(post(END_POINT)
-                   .contentType(MediaType.APPLICATION_JSON)
-                   .content(json)
-                   .accept(MediaType.APPLICATION_JSON)
-               ).andDo(print())
-               .andExpect(status().isCreated())
-               .andExpect(jsonPath("$").doesNotExist());
+          if (TransactionAction.TOTAL.equals(transactionAction)) {
+            continue;
           }
+          final TransactionAmountDto transactionAmount = new TransactionAmountDto(transactionAction, 1000L);
+          final RequestCreateTransactionDto createTransactionDto = new RequestCreateTransactionDto(1L, now, "테스트 이벤트", transactionAmount, category, "테스트 메모");
+          
+          doNothing().when(transactionWriteService).save(createTransactionDto);
+          
+          final String json = mapper.writeValueAsString(createTransactionDto);
+          
+          mvc.perform(post(END_POINT)
+                 .contentType(MediaType.APPLICATION_JSON)
+                 .content(json)
+                 .accept(MediaType.APPLICATION_JSON)
+             ).andDo(print())
+             .andExpect(status().isCreated())
+             .andExpect(jsonPath("$").doesNotExist());
         }
       }
       
       @Test
       void 내역_생성_거래_자산_유형() throws Exception {
         for (final TransactionCategory transactionCategory : TransactionCategory.values()) {
-          final RequestCreateTransactionDto createTransactionDto = new RequestCreateTransactionDto(1L, now, "테스트 이벤트", action, 1000L, transactionCategory, "테스트 메모");
+          final TransactionAmountDto transactionAmount = new TransactionAmountDto(action, 1000L);
+          final RequestCreateTransactionDto createTransactionDto = new RequestCreateTransactionDto(1L, now, "테스트 이벤트", transactionAmount, transactionCategory, "테스트 메모");
           doNothing().when(transactionWriteService).save(createTransactionDto);
           
           final String json = mapper.writeValueAsString(createTransactionDto);
@@ -173,8 +176,9 @@ class TransactionControllerTest {
   @Nested
   @DisplayName("실패 케이스")
   class 실패 {
-    TransactionAction action = TransactionAction.RECEIVING;
-    TransactionCategory category = TransactionCategory.ETC;
+    TransactionAction action;
+    TransactionCategory category;
+    FailTransactionAmountDto transactionAmount;
     
     @Nested
     @DisplayName("내역_생성_실패")
@@ -184,6 +188,9 @@ class TransactionControllerTest {
       @BeforeEach
       void 초기값() {
         now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        transactionAmount = new FailTransactionAmountDto(action, 1000L);
+        action = TransactionAction.RECEIVING;
+        category = TransactionCategory.ETC;
       }
       
       private void callApiForBadRequestWhenCreate(final FailTransactionWithScheduleDto dto) throws Exception {
@@ -198,79 +205,90 @@ class TransactionControllerTest {
       
       @Test
       void 내역_대상_아이디_NULL() throws Exception {
-        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(null, now, "event", action, 1000L, category, "memo");
+        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(null, now, "event", transactionAmount, category, "memo");
         callApiForBadRequestWhenCreate(dto);
       }
       
       @Test
       void 내역_날짜_NULL() throws Exception {
-        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, null, "event", action, 1000L, category, "memo");
+        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, null, "event", transactionAmount, category, "memo");
         callApiForBadRequestWhenCreate(dto);
       }
       
       @Test
       void 내역_행사_이름_NULL() throws Exception {
-        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, null, action, 1000L, category, "memo");
+        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, null, transactionAmount, category, "memo");
+        callApiForBadRequestWhenCreate(dto);
+      }
+      
+      @Test
+      void 내역_금액_객체_NULL() throws Exception {
+        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", null, category, "memo");
         callApiForBadRequestWhenCreate(dto);
       }
       
       @Test
       void 내역_거래_종류_NULL() throws Exception {
-        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", null, 1000L, category, "memo");
+        transactionAmount = new FailTransactionAmountDto(null, 1000L);
+        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", transactionAmount, category, "memo");
         callApiForBadRequestWhenCreate(dto);
       }
       
       @Test
       void 내역_거래_금액_NULL() throws Exception {
-        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", action, null, category, "memo");
+        transactionAmount = new FailTransactionAmountDto(action, null);
+        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", transactionAmount, category, "memo");
         callApiForBadRequestWhenCreate(dto);
       }
       
       @Test
       void 내역_거래_자산_종류_NULL() throws Exception {
-        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", action, 1000L, null, "memo");
+        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", transactionAmount, null, "memo");
         callApiForBadRequestWhenCreate(dto);
       }
       
       @Test
       void 내역_행사_이름_빈문자열() throws Exception {
-        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "", action, 1000L, category, "memo");
+        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "", transactionAmount, category, "memo");
         callApiForBadRequestWhenCreate(dto);
       }
       
       @Test
       void 내역_행사_이름_공백() throws Exception {
-        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, " ", action, 1000L, category, "memo");
+        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, " ", transactionAmount, category, "memo");
         callApiForBadRequestWhenCreate(dto);
       }
       
       @Test
       void 내역_메모_200자_초과() throws Exception {
-        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", action, 1000L, category, "m".repeat(201));
+        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", transactionAmount, category, "m".repeat(201));
         callApiForBadRequestWhenCreate(dto);
       }
       
       @Test
       void 내역_선물_종류_허용되지않은() throws Exception {
-        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", action, 1000L, "유희왕 카드", "memo");
+        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", transactionAmount, "유희왕 카드", "memo");
         callApiForBadRequestWhenCreate(dto);
       }
       
       @Test
       void 내역_거래_종류_허용되지않은() throws Exception {
-        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", "강탈", 1000L, category, "memo");
+        transactionAmount = new FailTransactionAmountDto("빈봉투 주기", 1000L);
+        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", transactionAmount, category, "memo");
         callApiForBadRequestWhenCreate(dto);
       }
       
       @Test
       void 내역_거래_금액_음수() throws Exception {
-        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", action, -1000L, category, "memo");
+        transactionAmount = new FailTransactionAmountDto(action, -1000L);
+        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", transactionAmount, category, "memo");
         callApiForBadRequestWhenCreate(dto);
       }
       
       @Test
       void 내역_거래_금액_초과() throws Exception {
-        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", action, Long.MAX_VALUE + 1L, category, "memo");
+        transactionAmount = new FailTransactionAmountDto(action, Long.MAX_VALUE + 1L);
+        final FailTransactionWithScheduleDto dto = new FailTransactionWithScheduleDto(1L, now, "event", transactionAmount, category, "memo");
         callApiForBadRequestWhenCreate(dto);
       }
       
@@ -285,6 +303,13 @@ class TransactionControllerTest {
     @Nested
     @DisplayName("내역_수정_실패")
     class 내역_수정_실패 {
+      @BeforeEach
+      void 초기값() {
+        action = TransactionAction.RECEIVING;
+        category = TransactionCategory.ETC;
+        transactionAmount = new FailTransactionAmountDto(action, 1000L);
+      }
+      
       private void callApiForBadRequestWhenUpdate(final FailTransactionDto dto) throws Exception {
         final String json = mapper.writeValueAsString(dto);
         final Long transactionId = 1L;
@@ -298,62 +323,69 @@ class TransactionControllerTest {
       
       @Test
       void 내역_대상_아이디_NULL() throws Exception {
-        final FailTransactionDto dto = new FailTransactionDto(null, action, 1000L, category, "memo");
+        final FailTransactionDto dto = new FailTransactionDto(null, transactionAmount, category, "memo");
         callApiForBadRequestWhenUpdate(dto);
       }
       
       @Test
       void 내역_금액_종류_NULL() throws Exception {
-        final FailTransactionDto dto = new FailTransactionDto(1L, null, 1000L, category, "memo");
+        transactionAmount = new FailTransactionAmountDto(null, 1000L);
+        final FailTransactionDto dto = new FailTransactionDto(1L, transactionAmount, category, "memo");
         callApiForBadRequestWhenUpdate(dto);
       }
       
       @Test
       void 내역_거래_금액_NULL() throws Exception {
-        final FailTransactionDto dto = new FailTransactionDto(1L, action, null, category, "memo");
+        transactionAmount = new FailTransactionAmountDto(action, null);
+        final FailTransactionDto dto = new FailTransactionDto(1L, transactionAmount, category, "memo");
         callApiForBadRequestWhenUpdate(dto);
       }
       
       @Test
       void 내역_거래_자산_종류_NULL() throws Exception {
-        final FailTransactionDto dto = new FailTransactionDto(1L, action, 1000L, null, "memo");
+        transactionAmount = new FailTransactionAmountDto(null, 1000L);
+        final FailTransactionDto dto = new FailTransactionDto(1L, transactionAmount, null, "memo");
         callApiForBadRequestWhenUpdate(dto);
       }
       
       @Test
       void 내역_메모_200자_초과() throws Exception {
-        final FailTransactionDto dto = new FailTransactionDto(1L, action, 1000L, category, "m".repeat(201));
+        final FailTransactionDto dto = new FailTransactionDto(1L, transactionAmount, category, "m".repeat(201));
         callApiForBadRequestWhenUpdate(dto);
       }
       
       @Test
       void 내역_선물_종류_허용되지않은() throws Exception {
-        final FailTransactionDto dto = new FailTransactionDto(1L, action, 1000L, "부루마블 지폐", "memo");
+        final FailTransactionDto dto = new FailTransactionDto(1L, transactionAmount, "부루마블 지폐", "memo");
         callApiForBadRequestWhenUpdate(dto);
       }
       
       @Test
       void 내역_거래_종류_허용되지않은() throws Exception {
-        final FailTransactionDto dto = new FailTransactionDto(1L, "줫다뺏음", 1000L, category, "memo");
+        transactionAmount = new FailTransactionAmountDto("줫다뺏음", 1000L);
+        final FailTransactionDto dto = new FailTransactionDto(1L, transactionAmount, category, "memo");
         callApiForBadRequestWhenUpdate(dto);
       }
       
       @Test
       void 내역_거래_금액_음수() throws Exception {
-        final FailTransactionDto dto = new FailTransactionDto(1L, action, -1000L, category, "memo");
+        transactionAmount = new FailTransactionAmountDto(action, -1000L);
+        final FailTransactionDto dto = new FailTransactionDto(1L, transactionAmount, category, "memo");
         callApiForBadRequestWhenUpdate(dto);
       }
       
       @Test
       void 내역_거래_초과() throws Exception {
-        final FailTransactionDto dto = new FailTransactionDto(1L, action, Long.MAX_VALUE + 1L, category, "memo");
+        transactionAmount = new FailTransactionAmountDto(action, Long.MAX_VALUE + 1L);
+        final FailTransactionDto dto = new FailTransactionDto(1L, transactionAmount, category, "memo");
         callApiForBadRequestWhenUpdate(dto);
       }
       
       @Test
       void 존재하지_않는_내역() throws Exception {
         doThrow(new CustomException(CustomErrorCode.NOT_FOUND, "수정할 내역을 찾을 수 없음")).when(transactionWriteService).patchTransactionById(any(RequestUpdateTransactionDto.class), anyLong());
-        final RequestUpdateTransactionDto transactionDto = new RequestUpdateTransactionDto(1L, action, 1000L, category, "memo");
+        final TransactionAmountDto transactionAmount = new TransactionAmountDto(action, 1000L);
+        final RequestUpdateTransactionDto transactionDto = new RequestUpdateTransactionDto(1L, transactionAmount, category, "memo");
         final String json = mapper.writeValueAsString(transactionDto);
         final Long transactionId = 1L;
         
@@ -367,7 +399,8 @@ class TransactionControllerTest {
       
       @Test
       void 잘못된_내역_아이디_문자열() throws Exception {
-        final RequestUpdateTransactionDto transactionDto = new RequestUpdateTransactionDto(1L, action, 1000L, category, "memo");
+        final TransactionAmountDto transactionAmount = new TransactionAmountDto(action, 1000L);
+        final RequestUpdateTransactionDto transactionDto = new RequestUpdateTransactionDto(1L, transactionAmount, category, "memo");
         final String json = mapper.writeValueAsString(transactionDto);
         
         mvc.perform(patch(END_POINT + "/test")
@@ -379,7 +412,8 @@ class TransactionControllerTest {
       
       @Test
       void 잘못된_내역_아이디_소수() throws Exception {
-        final RequestUpdateTransactionDto transactionDto = new RequestUpdateTransactionDto(1L, action, 1000L, category, "memo");
+        final TransactionAmountDto transactionAmount = new TransactionAmountDto(action, 1000L);
+        final RequestUpdateTransactionDto transactionDto = new RequestUpdateTransactionDto(1L, transactionAmount, category, "memo");
         final String json = mapper.writeValueAsString(transactionDto);
         
         mvc.perform(patch(END_POINT + "/0.03")
@@ -437,8 +471,7 @@ class FailTransactionWithScheduleDto {
   private final Object personId;
   private final Object eventDate;
   private final Object event;
-  private final Object action;
-  private final Object amount;
+  private final FailTransactionAmountDto transactionAmount;
   private final Object category;
   private final Object memo;
 }
@@ -447,8 +480,14 @@ class FailTransactionWithScheduleDto {
 @RequiredArgsConstructor
 class FailTransactionDto {
   private final Object personId;
-  private final Object action;
-  private final Object amount;
+  private final FailTransactionAmountDto transactionAmount;
   private final Object category;
   private final Object memo;
+}
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@RequiredArgsConstructor
+class FailTransactionAmountDto {
+  private final Object action;
+  private final Object amount;
 }
