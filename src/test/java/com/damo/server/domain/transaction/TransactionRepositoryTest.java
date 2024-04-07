3c073183886ee429293fcc1dd0fd14d6920b9bb9
Kim minho <klaus9267@gmail.com>
2024-04-07 23:34:17 +0900

    Test: 내역 repository 성공 커스텀 조회 케이스 작성

--- src/test/java/com/damo/server/domain/transaction/TransactionRepositoryTest.java
+++ src/test/java/com/damo/server/domain/transaction/TransactionRepositoryTest.java
@@ -2,11 +2,16 @@ package com.damo.server.domain.transaction;
 
 import com.damo.server.application.security.provider.OAuthProviderType;
 import com.damo.server.domain.common.exception.ExceptionThrowHelper;
+import com.damo.server.domain.common.pagination.param.TransactionPaginationParam;
 import com.damo.server.domain.person.entity.Person;
 import com.damo.server.domain.person.entity.PersonRelation;
 import com.damo.server.domain.person.repository.PersonRepository;
+import com.damo.server.domain.schedule.ScheduleRepository;
 import com.damo.server.domain.schedule.entity.Schedule;
-import com.damo.server.domain.schedule.entity.ScheduleStatus;
+import com.damo.server.domain.transaction.dto.RequestCreateTransactionDto;
+import com.damo.server.domain.transaction.dto.TransactionAmountDto;
+import com.damo.server.domain.transaction.dto.TransactionTotalAmountDto;
+import com.damo.server.domain.transaction.dto.TransactionWithScheduleDto;
 import com.damo.server.domain.transaction.entity.Transaction;
 import com.damo.server.domain.transaction.entity.TransactionAction;
 import com.damo.server.domain.transaction.entity.TransactionAmount;
@@ -20,11 +25,13 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,33 +44,190 @@ class TransactionRepositoryTest {
   private PersonRepository personRepository;
   @Autowired
   private UserRepository userRepository;
+  @Autowired
+  private ScheduleRepository scheduleRepository;
 
   @Nested
   @DisplayName("성공 케이스")
   class 성공 {
     LocalDateTime now;
     Transaction transaction;
+    User user;
+    Person person;
+    Schedule schedule;
 
     @BeforeEach
     void 초기값() {
       now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
 
-      User user = new User(1L, "name", "email", UserRole.USER, "username", "imageUrl", OAuthProviderType.KAKAO, "provideId");
-      User savedUser = userRepository.save(user);
+      final User newUser = new User(null, "name", "email", UserRole.USER, "username", "imageUrl", OAuthProviderType.KAKAO, "provideId");
+      user = userRepository.save(newUser);
 
-      Person person = new Person(1L, "name", PersonRelation.ETC, "01012341234", "memo", null, null, savedUser);
-      Person savedPerson = personRepository.save(person);
+      final Person newPerson = new Person(null, "name", PersonRelation.ETC, "01012341234", "memo", null, null, user);
+      person = personRepository.save(newPerson);
 
-      Schedule schedule = new Schedule(1L, "event", now, "memo", ScheduleStatus.NORMAL, null, null, savedUser, null);
-      TransactionAmount amount = new TransactionAmount(TransactionAction.GIVING, 1000L);
+      final TransactionAmountDto amountDto = new TransactionAmountDto(TransactionAction.GIVING, 1000L);
+      final RequestCreateTransactionDto transactionDto = new RequestCreateTransactionDto(person.getId(), now, "event", amountDto, TransactionCategory.ETC, "memo");
 
-      transaction = new Transaction(1L, amount, "memo", null, null, TransactionCategory.ETC, savedPerson, savedUser, schedule);
-      transaction = transactionRepository.save(transaction);
+      transaction = transactionRepository.save(Transaction.from(transactionDto, user.getId()));
+      schedule = transaction.getSchedule();
     }
 
     @Test
-    void 내역_생성() {
-      assertThat(transaction).extracting(
+    void 내역_생성_선물_종류() {
+      final TransactionAmount amount = new TransactionAmount(TransactionAction.GIVING, 1000L);
+      final TransactionCategory[] categories = TransactionCategory.values();
+
+      for (final TransactionCategory category : categories) {
+        final Transaction newTransaction = new Transaction(null, amount, category.getKey(), null, null, category, person, user, null);
+        final Transaction savedTransaction = transactionRepository.save(newTransaction);
+
+        assertCreateTransaction(newTransaction, savedTransaction);
+      }
+    }
+
+    @Test
+    void 내역_생성_거래_종류() {
+      final TransactionAction[] actions = TransactionAction.values();
+
+      for (final TransactionAction action : actions) {
+        if (TransactionAction.TOTAL.equals(action)) {
+          continue;
+        }
+        final TransactionAmount amount = new TransactionAmount(action, 1000L);
+        final Transaction newTransaction = new Transaction(null, amount, action.getKey(), null, null, TransactionCategory.ETC, person, user, null);
+        final Transaction savedTransaction = transactionRepository.save(newTransaction);
+
+        assertCreateTransaction(newTransaction, savedTransaction);
+      }
+    }
+
+    @Test
+    void 내역_조회_단건() {
+      final Transaction foundTransaction = transactionRepository.findById(transaction.getId()).orElseThrow(ExceptionThrowHelper.throwNotFound("조회할 거래 내역을 찾을 수 없음"));
+      assertReadTransaction(foundTransaction);
+    }
+
+    @Test
+    void 내역_조회_existsByEventDateAndPersonIdAndEvent() {
+      final Boolean isTransactionExists = transactionRepository.existsByEventDateAndPersonIdAndEvent(schedule.getEventDate(), person.getId(), schedule.getEvent());
+      assertThat(isTransactionExists).isTrue();
+    }
+
+    @Test
+    void 내역_조회_findByIdAndUserId() {
+      final Transaction foundTransaction = transactionRepository.findByIdAndUserId(transaction.getId(), user.getId()).orElseThrow(ExceptionThrowHelper.throwNotFound("조회할 거래 내역을 찾을 수 없음"));
+      assertReadTransaction(foundTransaction);
+    }
+
+    @Test
+    void 내역_조회_findAllByUserId() {
+      List<Transaction> transactions = new ArrayList<>();
+      for (int i = 0; i < 10; i++) {
+        final TransactionAmountDto amountDto = new TransactionAmountDto(TransactionAction.GIVING, 1000L);
+        final RequestCreateTransactionDto transactionDto = new RequestCreateTransactionDto(person.getId(), now, "event" + i, amountDto, TransactionCategory.ETC, "memo");
+        transactions.add(Transaction.from(transactionDto, user.getId()));
+      }
+      transactionRepository.saveAll(transactions);
+
+      final TransactionPaginationParam paginationParam = new TransactionPaginationParam(0, 10, TransactionAction.TOTAL, null, null, null, null);
+      final Page<TransactionWithScheduleDto> transactionWithScheduleDtoPage = transactionRepository.findAllByUserId(paginationParam.toPageable(), user.getId(), null, null, TransactionAction.TOTAL);
+      final List<Transaction> foundTransactions = transactionRepository.findAll();
+
+      assertThat(transactionWithScheduleDtoPage.getTotalPages()).isEqualTo((int) Math.ceil((double) foundTransactions.size() / 10));
+      assertThat(transactionWithScheduleDtoPage.getTotalElements()).isEqualTo(foundTransactions.size());
+    }
+
+    @Test
+    void 내역_조회_findTotalAmount() {
+      List<Transaction> transactions = new ArrayList<>();
+      for (int i = 0; i < 100; i++) {
+        final Random random = new Random();
+        final long randomAmount = random.nextLong(1000000);
+        final int randomIndex = random.nextInt(2) + 1;
+
+        final TransactionAmountDto amountDto = new TransactionAmountDto(TransactionAction.values()[randomIndex], randomAmount);
+        final RequestCreateTransactionDto transactionDto = new RequestCreateTransactionDto(person.getId(), now, "event" + i, amountDto, TransactionCategory.ETC, "memo");
+
+        transactions.add(Transaction.from(transactionDto, user.getId()));
+      }
+      transactionRepository.saveAll(transactions);
+
+      final List<Transaction> foundTransactions = transactionRepository.findAll();
+
+      final Map<TransactionAction, Long> amountMap = transactions.stream()
+          .collect(Collectors.groupingBy(
+              transaction -> transaction.getTransactionAmount().getAction(),
+              Collectors.summingLong(transaction -> transaction.getTransactionAmount().getAmount())
+          ));
+
+      if (TransactionAction.GIVING.equals(transaction.getTransactionAmount().getAction())) {
+        final Long newAmount = amountMap.get(TransactionAction.GIVING) + transaction.getTransactionAmount().getAmount();
+        amountMap.put(TransactionAction.GIVING, newAmount);
+      } else {
+        final Long newAmount = amountMap.get(TransactionAction.RECEIVING) + transaction.getTransactionAmount().getAmount();
+        amountMap.put(TransactionAction.RECEIVING, newAmount);
+      }
+
+      final TransactionTotalAmountDto totalAmountDto = transactionRepository.findTotalAmount(user.getId());
+
+      assertThat(totalAmountDto.getTotalGivingAmount()).isEqualTo(amountMap.get(TransactionAction.GIVING));
+      assertThat(totalAmountDto.getTotalReceivingAmount()).isEqualTo(amountMap.get(TransactionAction.RECEIVING));
+    }
+
+    @Test
+    void 내역_조회_readRecentAmounts() {
+      List<Transaction> transactions = new ArrayList<>();
+      final LocalDateTime startedAt = LocalDateTime.now().minusMonths(2);
+      for (int i = 0; i < 100; i++) {
+        final Random random = new Random();
+        final long randomAmount = random.nextLong(1000000);
+        final int randomIndex = random.nextInt(2) + 1;
+        final LocalDateTime randomDateTime = now.minusDays(random.nextInt(100) + 30);
+
+        final TransactionAmountDto amountDto = new TransactionAmountDto(TransactionAction.values()[randomIndex], randomAmount);
+        final RequestCreateTransactionDto transactionDto = new RequestCreateTransactionDto(person.getId(), randomDateTime, "event" + i, amountDto, TransactionCategory.ETC, "memo");
+
+        transactions.add(Transaction.from(transactionDto, user.getId()));
+      }
+      transactionRepository.saveAll(transactions);
+
+      final List<Transaction> foundTransactions = transactionRepository.findAll();
+
+      final Map<TransactionAction, Long> amountMap = transactions.stream()
+          .filter(data -> data.getSchedule().getEventDate().isAfter(startedAt))
+          .collect(Collectors.groupingBy(
+              transaction -> transaction.getTransactionAmount().getAction(),
+              Collectors.summingLong(transaction -> transaction.getTransactionAmount().getAmount())
+          ));
+
+      if (TransactionAction.GIVING.equals(transaction.getTransactionAmount().getAction())) {
+        final Long newAmount = amountMap.get(TransactionAction.GIVING) + transaction.getTransactionAmount().getAmount();
+        amountMap.put(TransactionAction.GIVING, newAmount);
+      } else {
+        final Long newAmount = amountMap.get(TransactionAction.RECEIVING) + transaction.getTransactionAmount().getAmount();
+        amountMap.put(TransactionAction.RECEIVING, newAmount);
+      }
+
+      final TransactionTotalAmountDto totalAmountDto = transactionRepository.readRecentAmounts(user.getId(), startedAt);
+
+      assertThat(totalAmountDto.getTotalGivingAmount()).isEqualTo(amountMap.get(TransactionAction.GIVING));
+      assertThat(totalAmountDto.getTotalReceivingAmount()).isEqualTo(amountMap.get(TransactionAction.RECEIVING));
+    }
+
+    @Test
+    void 내역_삭제() {
+      final Optional<Transaction> foundTransaction = transactionRepository.findById(transaction.getId());
+      assertThat(foundTransaction.isPresent()).isTrue();
+
+      transactionRepository.deleteById(transaction.getId());
+
+      final Optional<Transaction> deletedTransaction = transactionRepository.findById(transaction.getId());
+      assertThat(deletedTransaction.isPresent()).isFalse();
+    }
+
+    private void assertReadTransaction(final Transaction foundTransaction) {
+      assertThat(foundTransaction).extracting(
               Transaction::getId,
               Transaction::getMemo,
               Transaction::getCategory,
@@ -79,27 +243,21 @@ class TransactionRepositoryTest {
           );
     }
 
-    @Test
-    void 내역_조회() {
-      Transaction foundTransaction = transactionRepository.findById(transaction.getId()).orElseThrow(ExceptionThrowHelper.throwNotFound("조회할 거래 내역을 찾을 수 없음"));
-
-      assertThat(foundTransaction.getId()).isEqualTo(transaction.getId());
-      assertThat(foundTransaction.getTransactionAmount()).isEqualTo(transaction.getTransactionAmount());
-      assertThat(foundTransaction.getMemo()).isEqualTo(transaction.getMemo());
-      assertThat(foundTransaction.getCategory()).isEqualTo(transaction.getCategory());
-      assertThat(foundTransaction.getUser()).isEqualTo(transaction.getUser());
-      assertThat(foundTransaction.getPerson()).isEqualTo(transaction.getPerson());
-    }
-
-    @Test
-    void 내역_삭제() {
-      Optional<Transaction> foundTransaction1 = transactionRepository.findById(transaction.getId());
-      assertThat(foundTransaction1.isPresent()).isEqualTo(true);
-
-      transactionRepository.deleteById(transaction.getId());
-
-      Optional<Transaction> foundTransaction2 = transactionRepository.findById(transaction.getId());
-      assertThat(foundTransaction2.isPresent()).isEqualTo(false);
+    private void assertCreateTransaction(final Transaction newTransaction, final Transaction savedTransaction) {
+      assertThat(newTransaction).extracting(
+              Transaction::getId,
+              Transaction::getMemo,
+              Transaction::getCategory,
+              Transaction::getUser,
+              Transaction::getPerson
+          )
+          .containsExactly(
+              savedTransaction.getId(),
+              savedTransaction.getMemo(),
+              savedTransaction.getCategory(),
+              savedTransaction.getUser(),
+              savedTransaction.getPerson()
+          );
     }
   }
 }
